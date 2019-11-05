fb2a4c90f6ce823a28168bb36e2636e46effc8bf
Rachel Puder <rcompuder@gmail.com>
2019-11-05 10:09:45 -0500

    partial code

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var letters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var playerGuess=[]
+var guesses= 9
+
+letterGuess=letters[Math.random()*letters.length)]
+
+function clean(linput){
+
+
+
+}
