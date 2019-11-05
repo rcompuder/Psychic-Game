8fa4e7cb566765f719dc6d78c8606427337ff939
Rachel Puder <rcompuder@gmail.com>
2019-11-05 11:12:03 -0500

    fixed some js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,26 @@
-var letters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var playerGuess=[]
-var guessesLeft= 9
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var playerGuess;
+var guessesLeft = 9;
 
-letterAnswer=letters[Math.random()*letters.length];
+letterAnswer = letters[Math.floor(Math.random() * letters.length)];
 
-function clean(input){
-    letter= letter.tolowerCase();
-    letter.chatAt(0)
-    return letter
+function cleanInput(input) {
+    var letter;
+    letter = letter.tolowerCase();
+    letter.chatAt(0);
+    return letter;
 }
 
-while ((0< guessesLeft) && (playerGuess !== letterAnswer)){
-    playerGuess= prompt("What letter am I thinkin of?")
+while ((playerGuess !== letterAnswer) && (guessesLeft > 0)) {
+    playerGuess = prompt("What letter am I thinkin of?");
+    playerGuess = cleanInput(playerGuess);
+    document.querySelector("#guesses").innerHTML += (playerGuess + " ");
+    guessesLeft--;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+};
+if (playerGuess === letterAnswer) {
+    alert("You guessed the correct answer... YOU WON!");
+
+} else {
+    alert("You lose!");
 }
