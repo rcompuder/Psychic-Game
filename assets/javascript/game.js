9ffd4f5f98bd477aab8eea16095d8c8d60b0b34f
Rachel Puder <rcompuder@gmail.com>
2019-11-05 10:18:04 -0500

    added java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,15 @@
 var letters= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var playerGuess=[]
-var guesses= 9
+var guessesLeft= 9
 
-letterGuess=letters[Math.random()*letters.length)]
+letterAnswer=letters[Math.random()*letters.length];
 
-function clean(linput){
+function clean(input){
+    letter= letter.tolowerCase();
+    letter.chatAt(0)
+    return letter
+}
 
-
-
+while ((0< guessesLeft) && (playerGuess !== letterAnswer)){
+    playerGuess= prompt("What letter am I thinkin of?")
 }
